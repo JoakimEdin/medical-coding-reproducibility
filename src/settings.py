3b712c2463daaf7d673ac6c4d7a869648f77d91f
Joakim <je@corti.ai>
2023-02-17 07:57:01 +0000

    update configs

--- src/settings.py
+++ src/settings.py
@@ -9,10 +9,10 @@ TARGET_COLUMN = "target"
 SUBJECT_ID_COLUMN = "subject_id"
 
 DOWNLOAD_DIRECTORY_MIMICIII = (
-    "/data/je/mimiciii/1.4"  # Path to the MIMIC-III data. Example: ~/mimiciii/1.4
+    "path/to/mimiciii"  # Path to the MIMIC-III data. Example: ~/mimiciii/1.4
 )
-DOWNLOAD_DIRECTORY_MIMICIV = "/data/je/physionet.org/files/mimiciv/2.2"  # Path to the MIMIC-IV data. Example: ~/physionet.org/files/mimiciv/2.2
-DOWNLOAD_DIRECTORY_MIMICIV_NOTE = "/data/je/physionet.org/files/mimic-iv-note/2.2"  # Path to the MIMIC-IV-Note data. Example: ~/physionet.org/files/mimic-iv-note/2.2
+DOWNLOAD_DIRECTORY_MIMICIV = "path/to/mimiciv"  # Path to the MIMIC-IV data. Example: ~/physionet.org/files/mimiciv/2.2
+DOWNLOAD_DIRECTORY_MIMICIV_NOTE = "path/to/mimiciv-note"  # Path to the MIMIC-IV-Note data. Example: ~/physionet.org/files/mimic-iv-note/2.2
 
 
 DATA_DIRECTORY_MIMICIII_FULL = OmegaConf.load("configs/data/mimiciii_full.yaml").dir
@@ -21,8 +21,8 @@ DATA_DIRECTORY_MIMICIII_CLEAN = OmegaConf.load("configs/data/mimiciii_clean.yaml
 DATA_DIRECTORY_MIMICIV_ICD9 = OmegaConf.load("configs/data/mimiciv_icd9.yaml").dir
 DATA_DIRECTORY_MIMICIV_ICD10 = OmegaConf.load("configs/data/mimiciv_icd10.yaml").dir
 
-PROJECT = "joakim_edin/automatic-medical-coding"
-EXPERIMENT_DIR = "/data/je/experiments"  # Path to the experiment directory. Example: ~/experiments
+PROJECT = "<your project name>" # this variable is used for genersating plots and tables from wandb
+EXPERIMENT_DIR = "files/"  # Path to the experiment directory. Example: ~/experiments
 PALETTE = {
     "PLM-ICD": "#E69F00",
     "LAAT": "#009E73",
